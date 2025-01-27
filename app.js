272af14cca41b3f94dafb965f86aee44eae2f8a3
anshsingh06 <anshsingh01227@gmail.com>
2025-01-27 22:07:55 +0530

    js file

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const mongoose = require ("mongoose");
 // const Chat = require("./models/chats.js");
 
 
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+.then(()=>{
+    console.log("connected to DB");
+})
+.catch ((err)=> console.log(err));
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
 
 app.get("/", (req,res)=>{
     res.send("Hi,I am root");
